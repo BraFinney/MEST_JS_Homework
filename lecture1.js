90834efd6d3bd3be7e2e3f7fbd32d0696830cfb3
Charles <micnkru@gmail.com>
2016-10-24 16:11:57 +0000

    init change

--- lecture1.js
+++ lecture1.js
@@ -3,22 +3,31 @@
  */
 
 var 1stCar = 'Doge';
+//Invalid because you cannot start a name with a number
 var var = 'variable';
+//Invalid because var is a reserved word which cannot be used as variables
 var first name = 'Jerry';
+//Invalid because there should not be whitespace in variable naming
 var Sch00lName = 'MEST';
+//Valid because its in camelCase and you can use a number is allowed
+//since it doesnt begin
 var totalnumber = 100;
+//invalid because it is not Human readable
 
 /*
  * 2. Are the following variable valid? If it is not please explain it and correct them by using the best practise
  */
 var greeting  =  'It's nice to me you today!';
-var response = "Ashwin said, "Yes Sir!"";
+//Invalid because in cases like this you cannot use single quotes
+var response = "Ashwin said,  "Yes Sir!"";
+//Invalid because you are the "Yes Sir!" is not in quotation marks.  
 
 
 /*
  * 3. Try 0.3 + 0.1 === 0.4, explain what happen here
- */
 
+ */
+console.log(0.3 + 0.1 === 0.4)
 
 /*
  * 4. Build a pizza object by using all three object creation method, it must has at least 4 properties
